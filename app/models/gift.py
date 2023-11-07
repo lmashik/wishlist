c8389d53f18f65183ad6e1e013a2d45ab46de29e
Мария Лапикова <mashik_p@mail.ru>
2023-11-08 01:12:11 +0300

    Add comment field to Gift model and make migration

--- app/models/gift.py
+++ app/models/gift.py
@@ -1,7 +1,8 @@
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Text
 
 from app.core.db import Base
 
 
 class Gift(Base):
     name = Column(String(100), nullable=False)
+    comment = Column(Text)
