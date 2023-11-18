712dbf4dfbdd39116efeeef89d1c90a6152037e6
Мария Лапикова <mashik_p@mail.ru>
2023-11-18 21:47:05 +0300

    Create response Gift schema

--- api/routes/gift.py
+++ api/routes/gift.py
@@ -1,12 +1,16 @@
 from fastapi import APIRouter
 
 from api.crud.gift import create_gift
-from api.schemas.gift import GiftCreate
+from api.schemas.gift import GiftCreate, GiftDB
 
 router = APIRouter()
 
 
-@router.post('/')
+@router.post(
+    '/',
+    response_model=GiftDB,
+    response_model_exclude_none=True,
+)
 async def create_new_gift(
         gift: GiftCreate,
 ):
--- api/schemas/gift.py
+++ api/schemas/gift.py
@@ -5,13 +5,27 @@ from pydantic import BaseModel, Field
 from api.constants import NAME_MAX_LENGTH, NAME_MIN_LENGTH
 
 
-class GiftCreate(BaseModel):
-    name: str = Field(
-        ...,
-        title='Название',
+class GiftBase(BaseModel):
+    name: Optional[str] = Field(
+        None,
         min_length=NAME_MIN_LENGTH,
         max_length=NAME_MAX_LENGTH,
     )
     comment: Optional[str]
     link: Optional[str]
     price: Optional[int]
+
+
+class GiftCreate(GiftBase):
+    name: str = Field(
+        ...,
+        min_length=NAME_MIN_LENGTH,
+        max_length=NAME_MAX_LENGTH,
+    )
+
+
+class GiftDB(GiftCreate):
+    id: str
+
+    class Config:
+        from_attributes = True
